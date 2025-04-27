66929e3fa1a98cc42f41aa3dace23f4cc761556a
Alok Kumar <alok.kumar@ovyo.com>
2025-04-27 17:07:00 +0530

    Initial Commit

---
+++ AudioBook/AudioBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioBookApp.swift
+//  AudioBook
+//
+//  Created by Alok Kumar on 27/04/25.
+//
+
+import SwiftUI
+
+@main
+struct AudioBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
